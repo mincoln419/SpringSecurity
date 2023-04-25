22aed22ee0864c43171b7133a0c5cf949d456ca9
mermer <mincoln419@naver.com>
2023-04-25 10:03:30 +0900

    oauth2

---
+++ mermer/src/main/java/com/oauth/mermer/MermerApplication.java
@@ -0,0 +1,13 @@
+package com.oauth.mermer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MermerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MermerApplication.class, args);
+	}
+
+}
---
+++ mermer/src/test/java/com/oauth/mermer/MermerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oauth.mermer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MermerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
