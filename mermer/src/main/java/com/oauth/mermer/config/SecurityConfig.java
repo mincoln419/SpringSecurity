12e3bd8c69cc7cb22eebd612e3f8f625dea972a0
mermer <mincoln419@naver.com>
2023-04-27 17:29:34 +0900

    custom entry point

--- mermer/src/main/java/com/oauth/mermer/config/SecurityConfig.java
+++ mermer/src/main/java/com/oauth/mermer/config/SecurityConfig.java
@@ -8,6 +8,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
@@ -20,7 +21,8 @@ public class SecurityConfig {
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		http.authorizeHttpRequests().anyRequest().authenticated();
 //		http.formLogin();
-		http.httpBasic();
+		http.httpBasic();//.authenticationEntryPoint(new CustomAuthenticationEntryPoint());
+		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 //		http.exceptionHandling().authenticationEntryPoint(new AuthenticationEntryPoint() {
 //
 //			@Override
