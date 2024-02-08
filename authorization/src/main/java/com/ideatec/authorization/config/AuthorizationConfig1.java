f1643fab96a090e3677dcd341fb589c372481828
mingeon cho <mincoln419@naver.com>
2024-02-08 17:42:05 +0900

    authorization server init

---
+++ authorization/src/main/java/com/ideatec/authorization/config/AuthorizationConfig1.java
@@ -0,0 +1,53 @@
+package com.ideatec.authorization.config;
+
+import java.util.UUID;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
+import org.springframework.security.oauth2.server.authorization.client.InMemoryRegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
+import org.springframework.security.oauth2.server.authorization.settings.AuthorizationServerSettings;
+import org.springframework.security.oauth2.server.authorization.settings.ClientSettings;
+
+@Configuration
+@Import(OAuth2AuthorizationServerConfiguration.class)
+public class AuthorizationConfig1 {
+
+
+
+	@Bean
+	public AuthorizationServerSettings providerSettings() {
+
+		return AuthorizationServerSettings.builder()
+				.issuer("http://localhost:90000")
+				.build();
+	}
+
+	@Bean
+	public RegisteredClientRepository registeredClientRepository() {
+		RegisteredClient registeredClient = RegisteredClient.withId(UUID.randomUUID().toString())
+				.clientId("oauth2-client-app")
+				.clientSecret("{noop}-secret")
+				.clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+				.clientAuthenticationMethod(ClientAuthenticationMethod.NONE)
+				.authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+				.authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
+				.authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS)
+				.redirectUri("http://127.0.0.1:28081/login/oauth2/code/oauth2-client-app")
+				.redirectUri("http://127.0.0.1:28081")
+				.scope("message.read")
+				.scope("message.write")
+				.clientSettings(ClientSettings.builder().requireAuthorizationConsent(true).build())
+				.build();
+
+		InMemoryRegisteredClientRepository registeredClientRepository = new InMemoryRegisteredClientRepository(registeredClient);
+		return registeredClientRepository;
+
+	}
+
+}
---
+++ authorization/src/main/java/com/ideatec/authorization/config/AuthorizationConfig2.java
@@ -0,0 +1,10 @@
+package com.ideatec.authorization.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AuthorizationConfig2 {
+	
+	
+
+}
