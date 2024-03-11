505b214f12adcfaf343b953773286c3573f3f066
mermer vivo <mincoln419@naver.com>
2024-03-11 10:13:26 +0900

    resource run pj add

---
+++ resrouce-run/src/main/java/com/ideatec/resroucerun/ResrouceRunApplication.java
@@ -0,0 +1,13 @@
+package com.ideatec.resroucerun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResrouceRunApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResrouceRunApplication.class, args);
+    }
+
+}
---
+++ resrouce-run/src/test/java/com/ideatec/resroucerun/ResrouceRunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ideatec.resroucerun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResrouceRunApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
